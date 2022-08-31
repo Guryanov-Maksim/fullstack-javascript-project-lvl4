868310dac9c2903a232e98868f6fd6b660117e74
Maksim-Guryanov <guryanov-mv@yandex.ru>
2022-08-31 17:00:10 +0300

    fix: wrong property in the knex configuration

--- knexfile.js
+++ knexfile.js
@@ -24,7 +24,7 @@ export const production = {
   client: 'pg',
   useNullAsDefault: true,
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
   },
   migrations,
