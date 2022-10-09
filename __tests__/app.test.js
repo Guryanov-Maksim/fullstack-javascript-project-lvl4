b55d90108d5ef8222d290fb9752984f5e73a80d7
Maksim-Guryanov <guryanov-mv@yandex.ru>
2022-10-09 14:10:42 +0300

    feat: add tests for users crud, session and main page request

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,31 @@
-import { result } from 'lodash';
-import init from '../server/plugin.js';
+import fastify from 'fastify';
+import init from '../server/plugin';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('requests', () => {
+  let app;
+
+  beforeAll(async () => {
+    app = fastify();
+    await init(app);
+  });
+
+  test('GET 200', async () => {
+    const res = await app.inject({
+      method: 'GET',
+      url: app.reverse('root'),
+    });
+    expect(res.statusCode).toBe(200);
+  });
+
+  test('GET 404', async () => {
+    const res = await app.inject({
+      method: 'GET',
+      url: '/wrong-path',
+    });
+    expect(res.statusCode).toBe(404);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
 });
