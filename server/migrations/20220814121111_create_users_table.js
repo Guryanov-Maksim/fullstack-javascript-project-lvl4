be69e56e35c3ecd70e0bbb3bca3412774aa53f27
Maksim-Guryanov <guryanov-mv@yandex.ru>
2022-10-14 15:12:06 +0300

    refactor: split migrations for users and statuses into separate files

--- server/migrations/20220814121111_create_users_table.js
+++ server/migrations/20220814121111_create_users_table.js
@@ -1,25 +1,13 @@
 export const up = (knex) => (
-  Promise.all([
-    knex.schema.createTable('users', (table) => {
-      table.increments('id').primary();
-      table.string('first_name');
-      table.string('last_name');
-      table.string('email');
-      table.string('password_digest');
-      table.timestamp('created_at').defaultTo(knex.fn.now());
-      table.timestamp('updated_at').defaultTo(knex.fn.now());
-    }),
-    knex.schema.createTable('statuses', (table) => {
-      table.increments('id').primary();
-      table.string('name');
-      table.timestamp('created_at').defaultTo(knex.fn.now());
-    }),
-  ])
+  knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('first_name');
+    table.string('last_name');
+    table.string('email');
+    table.string('password_digest');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  })
 );
 
-export const down = (knex) => (
-  Promise.all([
-    knex.schema.dropTable('users'),
-    knex.schema.dropTable('statuses'),
-  ])
-);
+export const down = (knex) => knex.schema.dropTable('users');
