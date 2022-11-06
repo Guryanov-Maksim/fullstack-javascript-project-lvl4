285a5494b5a74870776c575fd7be207bae626bb9
Maksim-Guryanov <guryanov-mv@yandex.ru>
2022-11-06 14:42:06 +0300

    fix: error tracking

--- server/logging/index.js
+++ server/logging/index.js
@@ -1,9 +1,12 @@
 import Rollbar from 'rollbar';
 
 const rollbar = new Rollbar({
-  accessToken: 'b32f018d706f4343aba3eb56c93eec59',
+  accessToken: process.env.NODE_ENV,
   captureUncaught: true,
   captureUnhandledRejections: true,
+  payload: {
+    environment: 'production',
+  },
 });
 
 export default rollbar;
